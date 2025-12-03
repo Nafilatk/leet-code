763a2c431440109149cadb80a259f83a99d8e6f8
mjasil <nafilatk7@gmail.com>
2025-12-03 15:35:53 +0530

    leet10

--- leet11_56.js
+++ leet11_56.js
@@ -1,1 +1,14 @@
 
+function maxSubArray(nums) {
+  let current = 0;
+  let max = nums[0];
+
+  for (let n of nums) {
+    current += n;
+    max = Math.max(max, current);
+
+    if (current < 0) current = 0;
+  }
+
+  return max;
+}
